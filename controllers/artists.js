9056f3c0039ff6f4e65cac25a6be603b8b15e6d6
ifotn <rich@infrontofthenet.com>
2021-10-06 13:29:54 -0400

    l5/2 - read data

--- controllers/artists.js
+++ controllers/artists.js
@@ -4,9 +4,24 @@ let express = require('express')
 // instantiate an express router to parse and direct url requests
 let router = express.Router()
 
+// add model ref
+let Artist = require('../models/artist')
+
 // GET: /artists => show index view
 router.get('/', (req, res) => {
-    res.render('artists/index')
+    // use Artist model to fetch all documents from artists collection in mongodb
+    Artist.find((err, artists) => {
+        if (err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.render('artists/index', {
+                artists: artists,
+                title: 'Artists'
+            })
+        }
+    })
 })
 
 // make public
