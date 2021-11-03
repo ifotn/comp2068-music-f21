d6068541c402ce0ffa2f7f477dde5fd24384a09f
ifotn <rich@infrontofthenet.com>
2021-11-03 12:27:19 -0400

    l8/2 - registration

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// passport for auth
+const passport = require('passport')
+const User = require('../models/user')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node Tunes' });
@@ -14,5 +18,38 @@ router.get('/about', (req, res) => {
   })
 })
 
+// GET: /register
+router.get('/register', (req, res) => {
+  res.render('register', {
+    title: 'Register'
+  })
+})
+
+// POST: /register
+router.post('/register', (req, res) => {
+  // use User Model & passport to create a new user in MongoDB.  Send password separately so it can be hashed by passport
+  User.register(new User({ username: req.body.username }), req.body.password, (err, newUser) => {
+    if (err) {
+      console.log(err)
+      res.render('register', {
+        message: err
+      })
+    }
+    else {
+      // registration succeeded.  log user in and load main artist page
+      req.login(newUser, (err) => {
+        res.redirect('/artists')
+      })
+    }
+  })
+})
+
+
+// GET: /login
+router.get('/login', (req, res) => {
+  res.render('login', {
+    title: 'Login'
+  })
+})
 
 module.exports = router;
